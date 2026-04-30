eb567e784977d43526069fc9e6a720464cf6a523
Devanupriy Jain <devanupriyj@gmail.com>
2026-05-01 02:10:29 +0530

    fix port for deployment

--- server.js
+++ server.js
@@ -16,4 +16,5 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/novels", novelRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
