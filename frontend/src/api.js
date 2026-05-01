66fcb1f4ed5dffe4a7032dfc8f81f0d297528241
Devanupriy Jain <devanupriyj@gmail.com>
2026-05-01 10:11:26 +0530

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: "https://webnovel-a5ji.onrender.com/api",
 });
 
+// 🔥 attach token automatically
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = token;
+  }
+
+  return config;
+});
+
 export default api;
